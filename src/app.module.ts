d3db250d1710b435b579316b1cc29b387fcbb0e0
dmitriy <d.dobrenkiy@hotmail.com>
2021-07-09 22:16:20 +0300

    token

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TokenModule } from './token/token.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DBURL),
     UserModule,
+    AuthModule,
+    TokenModule,
   ],
   controllers: [AppController],
   providers: [AppService],
